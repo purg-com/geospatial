420a0f41d54cc000b1fa8218678d5146a1c01f4f
sysop <thecarnie@users.noreply.github.com>
2022-12-26 19:47:58 -0600

    Update addok.ex

--- lib/providers/addok.ex
+++ lib/providers/addok.ex
@@ -42,7 +42,7 @@ defmodule Geospatial.Providers.Addok do
   VOID implementation for `c:Geospatial.Providers.Provider.get_by_id/2`.
   """
   @spec get_by_id(String.t(), keyword()) :: list(Address.t())
-  defp get_by_id(_id, _options), do: []
+  def get_by_id(_id, _options), do: []
 
   @spec build_url(atom(), map(), list()) :: String.t()
   defp build_url(method, args, options) do
